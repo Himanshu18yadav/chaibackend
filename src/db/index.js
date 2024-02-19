afd31740e17625d6395a6542248e29b31fd003c6
Himanshu18yadav <113437927+Himanshu18yadav@users.noreply.github.com>
2024-02-19 19:48:54 +0530

    acces

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose";//it connect with database
 import {db_name} from"../constant.js";
 
-const dbconnect=async()=>{
+const connectdb=async()=>{
     try {
         const instances=await mongoose.connect(`${process.env.DATABASE_URI}/${db_name}`)
         console.log(`\n mongodb connected db host:${instances.connection.host}`)
         
     } catch (error) {
         console.log("IT'S SAD mongodb connection failed",error);
-        process.exit(1)
+        process.exit(1)//
         
     }
 
 }
-export default dbconnect;
+export default connectdb; 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 import{db_name}from "./constant.js";
+
 import 'dotenv/config'
 
 import dbconnect from"./db/index.js";
-dbconnect();
+import { app } from "./app.js";
+dbconnect()
+.then(()=>{
+    app.listen(process.env.PORT||4000,()=>{
+        console.log (`server is listening at port ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("mongodb failed",err);
+})
